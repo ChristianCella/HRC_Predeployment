bff21ee802f04a3859d0dac3d5968a8235d1d9e2
ChristianCella <christian.cella@polimi.it>
2024-07-19 20:38:06 +0200

    Fully functioning robot program. Before proceding in calculating the times, some functions will be made to make the code clearer

--- Predeployment_framework/main_C#.cs
+++ Predeployment_framework/main_C#.cs
@@ -11,6 +11,7 @@ This is the first attempt for a structured communication.
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Collections;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.IO;
@@ -127,7 +128,7 @@ class Program
                 	}
                 	else if (sequence[0, kk] == 0)
                 	{
-                		RobotPickPlace(robot_name, op_rob_name, kk + 1, output, verbose);
+                		RobotPickPlace(robot_name, obj_name, target_name, op_rob_name, kk + 1, output, verbose);
                 	}
                 }
                              
@@ -373,10 +374,104 @@ class Program
    		TxApplication.RefreshDisplay();
     }
 
-    static void RobotPickPlace(string robot_name, string op_name, int op_idx, StringWriter m_output, bool verbose)
+	// Function to add OLP commands
+	static void AddOpenCloseCommand(string point_name, string gripper_name, string pose_name, string tcp_name)
+	{
+		// Save the second point to close the gripper		
+		TxRoboticViaLocationOperation Waypoint =  TxApplication.ActiveDocument.
+  		GetObjectsByName(point_name)[0] as TxRoboticViaLocationOperation;
+  		
+  		// Save the gripper "Camozzi gripper" 	
+  		ITxObject Gripper = TxApplication.ActiveDocument.
+		GetObjectsByName(gripper_name)[0] as TxGripper;
+  		
+  		// Save the pose "Gripper Closed"  		
+  		ITxObject Pose = TxApplication.ActiveDocument.
+		GetObjectsByName(pose_name)[0] as TxPose;
+  		
+  		// Save the reference frame of the gripper 		
+  		ITxObject tGripper = TxApplication.ActiveDocument.
+		GetObjectsByName(tcp_name)[0] as TxFrame;
+		
+		// Create an array called "elements" and the command to be written in it
+    	ArrayList elements1 = new ArrayList();
+    	ArrayList elements2 = new ArrayList();
+    	ArrayList elements3 = new ArrayList();
+    	ArrayList elements4 = new ArrayList();
+    	ArrayList elements5 = new ArrayList();
+	
+    	var myCmd1 = new TxRoboticCompositeCommandStringElement("# Destination");
+    	var myCmd11 = new TxRoboticCompositeCommandTxObjectElement(Gripper);
+
+    	var myCmd2 = new TxRoboticCompositeCommandStringElement("# Drive");
+    	var myCmd21 = new TxRoboticCompositeCommandTxObjectElement(Pose);
+
+    	var myCmd3 = new TxRoboticCompositeCommandStringElement("# Destination");
+    	var myCmd31 = new TxRoboticCompositeCommandTxObjectElement(Gripper);
+
+    	var myCmd4 = new TxRoboticCompositeCommandStringElement("# WaitDevice");
+    	var myCmd41 = new TxRoboticCompositeCommandTxObjectElement(Pose);
+
+    	var myCmd5 = new TxRoboticCompositeCommandStringElement("# Grip"); // For the closing
+		if (pose_name == "OPEN")
+		{
+			myCmd5 = new TxRoboticCompositeCommandStringElement("# Release"); // For the closing
+		}
+    	var myCmd51 = new TxRoboticCompositeCommandTxObjectElement(tGripper);
+	
+ 		// First line of command	
+    	elements1.Add(myCmd1);
+    	elements1.Add(myCmd11);
+	  	
+    	TxRoboticCompositeCommandCreationData txRoboticCompositeCommandCreationData1 =
+    	new TxRoboticCompositeCommandCreationData(elements1);
+	
+    	Waypoint.CreateCompositeCommand(txRoboticCompositeCommandCreationData1);
+    	
+		// Second line of command
+    	elements2.Add(myCmd2);
+    	elements2.Add(myCmd21);
+
+    	TxRoboticCompositeCommandCreationData txRoboticCompositeCommandCreationData2 =
+    	new TxRoboticCompositeCommandCreationData(elements2);
+	
+    	Waypoint.CreateCompositeCommand(txRoboticCompositeCommandCreationData2);
+    	
+		// Third line of command
+    	elements3.Add(myCmd3);
+    	elements3.Add(myCmd31);
+
+    	TxRoboticCompositeCommandCreationData txRoboticCompositeCommandCreationData3 =
+    	new TxRoboticCompositeCommandCreationData(elements3);
+	
+    	Waypoint.CreateCompositeCommand(txRoboticCompositeCommandCreationData3);
+    	
+		// Fourth line of command
+    	elements4.Add(myCmd4);
+    	elements4.Add(myCmd41);
+
+    	TxRoboticCompositeCommandCreationData txRoboticCompositeCommandCreationData4 =
+    	new TxRoboticCompositeCommandCreationData(elements4);
+	
+    	Waypoint.CreateCompositeCommand(txRoboticCompositeCommandCreationData4);
+    	
+		// Fifth line of command	
+    	elements5.Add(myCmd5);
+    	elements5.Add(myCmd51);
+
+    	TxRoboticCompositeCommandCreationData txRoboticCompositeCommandCreationData5 =
+    	new TxRoboticCompositeCommandCreationData(elements5);
+	
+    	Waypoint.CreateCompositeCommand(txRoboticCompositeCommandCreationData5);
+       
+	}
+
+	// Function to create robot programs automatically
+    static void RobotPickPlace(string robot_name, string pick_target, string place_target, string op_name, 
+								int op_idx, StringWriter m_output, bool verbose)
     {
     	
-    	string new_tcp = "tcp_1";
+    	string new_tcp = "tf_tcp_1";
     	string new_motion_type = "MoveL";
 		string new_speed = "1000";
 		string new_accel = "1200";
@@ -385,7 +480,7 @@ class Program
 
         // Save the robot (the index may change)  	
     	TxObjectList objects = TxApplication.ActiveDocument.GetObjectsByName(robot_name);
-    	var robot = objects[0] as TxRobot;
+    	var robot = objects[1] as TxRobot;
     	   	
     	// Create the new operation    	
     	TxContinuousRoboticOperationCreationData data = new TxContinuousRoboticOperationCreationData(op_name + op_idx);
@@ -396,36 +491,59 @@ class Program
 
         // Create all the necessary points       
         TxRoboticViaLocationOperationCreationData Point1 = new TxRoboticViaLocationOperationCreationData();
-        Point1.Name = "point1" + op_idx; // First point
+        Point1.Name = "point1op" + op_idx; // First point
         
         TxRoboticViaLocationOperationCreationData Point2 = new TxRoboticViaLocationOperationCreationData();
-        Point2.Name = "point2" + op_idx; // Second point
+        Point2.Name = "point2op" + op_idx; // Second point
         
         TxRoboticViaLocationOperationCreationData Point3 = new TxRoboticViaLocationOperationCreationData();
-        Point3.Name = "point3" + op_idx; // Third point
+        Point3.Name = "point3op" + op_idx; // Third point
+
+		TxRoboticViaLocationOperationCreationData Point4 = new TxRoboticViaLocationOperationCreationData();
+        Point4.Name = "point4op" + op_idx; // Fourth point
+
+		TxRoboticViaLocationOperationCreationData Point5 = new TxRoboticViaLocationOperationCreationData();
+        Point5.Name = "point5op" + op_idx; // Fifth point
+
+		TxRoboticViaLocationOperationCreationData Point6 = new TxRoboticViaLocationOperationCreationData();
+        Point6.Name = "point6op" + op_idx; // Sixth point
+
+		TxRoboticViaLocationOperationCreationData Point7 = new TxRoboticViaLocationOperationCreationData();
+        Point7.Name = "point7op" + op_idx; // Seventh point
+
+		TxRoboticViaLocationOperationCreationData Point8 = new TxRoboticViaLocationOperationCreationData();
+        Point8.Name = "point8op" + op_idx; // Eighth point
 
         TxRoboticViaLocationOperation FirstPoint = MyOp.CreateRoboticViaLocationOperation(Point1);
         TxRoboticViaLocationOperation SecondPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point2, FirstPoint);
         TxRoboticViaLocationOperation ThirdPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point3, SecondPoint);
+		TxRoboticViaLocationOperation FourthPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point4, ThirdPoint);
+		TxRoboticViaLocationOperation FifthPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point5, FourthPoint);
+		TxRoboticViaLocationOperation SixthPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point6, FifthPoint);
+		TxRoboticViaLocationOperation SeventhPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point7, SixthPoint);
+		TxRoboticViaLocationOperation EighthPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point8, SeventhPoint);
 
-        // Impose a position to the new waypoint		
-		double rotVal = Math.PI;
-		TxTransformation rotX = new TxTransformation(new TxVector(rotVal, 0, 0), 
-		TxTransformation.TxRotationType.RPY_XYZ);
-		FirstPoint.AbsoluteLocation = rotX;
-		
-		var pointA = new TxTransformation(FirstPoint.AbsoluteLocation);
-		pointA.Translation = new TxVector(300, 0, 300);
-		FirstPoint.AbsoluteLocation = pointA;
+        // Put the first and the last point in the current robot TCP position (the correct frame)	
+		TxFrame TCPpose1 = TxApplication.ActiveDocument.
+		GetObjectsByName(new_tcp)[0] as TxFrame;
+		var TCP_pose1 = new TxTransformation(TCPpose1.LocationRelativeToWorkingFrame);
 
-        // Impose a position to the second waypoint		
+        FirstPoint.LocationRelativeToWorkingFrame = TCP_pose1;
+		EighthPoint.LocationRelativeToWorkingFrame = TCP_pose1;
+
+        // Impose a position to the second waypoint	(acquire the position of the object to be picked)
+		TxObjectList obj_pick = TxApplication.ActiveSelection.GetItems();
+		obj_pick = TxApplication.ActiveDocument.GetObjectsByName(pick_target + op_idx);
+		var obj = obj_pick[0] as ITxLocatableObject;
+		var position_pick = new TxTransformation(obj.AbsoluteLocation);
+
 		double rotVal2 = Math.PI;
 		TxTransformation rotX2 = new TxTransformation(new TxVector(rotVal2, 0, 0), 
 		TxTransformation.TxRotationType.RPY_XYZ);
 		SecondPoint.AbsoluteLocation = rotX2;
 		
 		var pointB = new TxTransformation(SecondPoint.AbsoluteLocation);
-		pointB.Translation = new TxVector(300, 0, 25);
+		pointB.Translation = new TxVector(position_pick[0, 3], position_pick[1, 3], position_pick[2, 3] + 100);
 		SecondPoint.AbsoluteLocation = pointB;
 		
 		// Impose a position to the third waypoint		
@@ -435,8 +553,53 @@ class Program
 		ThirdPoint.AbsoluteLocation = rotX3;
 		
 		var pointC = new TxTransformation(ThirdPoint.AbsoluteLocation);
-		pointC.Translation = new TxVector(300, 0, 300);
+		pointC.Translation = new TxVector(position_pick[0, 3], position_pick[1, 3], position_pick[2, 3]);
 		ThirdPoint.AbsoluteLocation = pointC;
+
+		// Impose a position to the fourth waypoint		
+		double rotVal4 = Math.PI;
+		TxTransformation rotX4 = new TxTransformation(new TxVector(rotVal4, 0, 0), 
+		TxTransformation.TxRotationType.RPY_XYZ);
+		FourthPoint.AbsoluteLocation = rotX4;
+		
+		var pointD = new TxTransformation(FourthPoint.AbsoluteLocation);
+		pointD.Translation = new TxVector(position_pick[0, 3], position_pick[1, 3], position_pick[2, 3] + 100);
+		FourthPoint.AbsoluteLocation = pointD;
+
+		// Impose a position to the fifth waypoint	(acquire the position of the object to be picked)
+		TxObjectList obj_place = TxApplication.ActiveSelection.GetItems();
+		obj_place = TxApplication.ActiveDocument.GetObjectsByName(place_target);
+		var obj_pl = obj_place[0] as ITxLocatableObject;
+		var position_place = new TxTransformation(obj_pl.AbsoluteLocation);
+
+		double rotVal5 = Math.PI;
+		TxTransformation rotX5 = new TxTransformation(new TxVector(rotVal5, 0, 0), 
+		TxTransformation.TxRotationType.RPY_XYZ);
+		FifthPoint.AbsoluteLocation = rotX5;
+		
+		var pointE = new TxTransformation(FifthPoint.AbsoluteLocation);
+		pointE.Translation = new TxVector(position_place[0, 3], position_place[1, 3], position_place[2, 3] + 100);
+		FifthPoint.AbsoluteLocation = pointE;
+
+		// Impose a position to the sixth waypoint		
+		double rotVal6 = Math.PI;
+		TxTransformation rotX6 = new TxTransformation(new TxVector(rotVal6, 0, 0), 
+		TxTransformation.TxRotationType.RPY_XYZ);
+		SixthPoint.AbsoluteLocation = rotX6;
+		
+		var pointF = new TxTransformation(SixthPoint.AbsoluteLocation);
+		pointF.Translation = new TxVector(position_place[0, 3], position_place[1, 3], position_place[2, 3]);
+		SixthPoint.AbsoluteLocation = pointF;
+
+		// Impose a position to the seventh waypoint		
+		double rotVal7 = Math.PI;
+		TxTransformation rotX7 = new TxTransformation(new TxVector(rotVal7, 0, 0), 
+		TxTransformation.TxRotationType.RPY_XYZ);
+		SeventhPoint.AbsoluteLocation = rotX7;
+		
+		var pointG = new TxTransformation(SeventhPoint.AbsoluteLocation);
+		pointG.Translation = new TxVector(position_place[0, 3], position_place[1, 3], position_place[2, 3] + 100);
+		SeventhPoint.AbsoluteLocation = pointG;
 
         // NOTE: you must associate the robot to the operation!
 		MyOp.Robot = robot; 
@@ -451,26 +614,65 @@ class Program
 		"ControllerName");
 
 		// Set the new parameters for the waypoint					
-		paramHandler.OnComplexValueChanged("Tool", new_tcp, FirstPoint);
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", FirstPoint);
 		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, FirstPoint);
         paramHandler.OnComplexValueChanged("Speed", new_speed, FirstPoint);
         paramHandler.OnComplexValueChanged("Accel", new_accel, FirstPoint);
 		paramHandler.OnComplexValueChanged("Blend", new_blend, FirstPoint);
 		paramHandler.OnComplexValueChanged("Coord Type", new_coord, FirstPoint);
 		
-		paramHandler.OnComplexValueChanged("Tool", new_tcp, SecondPoint);
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", SecondPoint);
 		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, SecondPoint);
         paramHandler.OnComplexValueChanged("Speed", new_speed, SecondPoint);
         paramHandler.OnComplexValueChanged("Accel", new_accel, SecondPoint);
 		paramHandler.OnComplexValueChanged("Blend", new_blend, SecondPoint);
 		paramHandler.OnComplexValueChanged("Coord Type", new_coord, SecondPoint);
 		
-		paramHandler.OnComplexValueChanged("Tool", new_tcp, ThirdPoint);
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", ThirdPoint);
 		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, ThirdPoint);
         paramHandler.OnComplexValueChanged("Speed", new_speed, ThirdPoint);
         paramHandler.OnComplexValueChanged("Accel", new_accel, ThirdPoint);
 		paramHandler.OnComplexValueChanged("Blend", new_blend, ThirdPoint);
 		paramHandler.OnComplexValueChanged("Coord Type", new_coord, ThirdPoint);
+
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", FourthPoint);
+		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, FourthPoint);
+        paramHandler.OnComplexValueChanged("Speed", new_speed, FourthPoint);
+        paramHandler.OnComplexValueChanged("Accel", new_accel, FourthPoint);
+		paramHandler.OnComplexValueChanged("Blend", new_blend, FourthPoint);
+		paramHandler.OnComplexValueChanged("Coord Type", new_coord, FourthPoint);
+
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", FifthPoint);
+		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, FifthPoint);
+        paramHandler.OnComplexValueChanged("Speed", new_speed, FifthPoint);
+        paramHandler.OnComplexValueChanged("Accel", new_accel, FifthPoint);
+		paramHandler.OnComplexValueChanged("Blend", new_blend, FifthPoint);
+		paramHandler.OnComplexValueChanged("Coord Type", new_coord, FifthPoint);
+
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", SixthPoint);
+		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, SixthPoint);
+        paramHandler.OnComplexValueChanged("Speed", new_speed, SixthPoint);
+        paramHandler.OnComplexValueChanged("Accel", new_accel, SixthPoint);
+		paramHandler.OnComplexValueChanged("Blend", new_blend, SixthPoint);
+		paramHandler.OnComplexValueChanged("Coord Type", new_coord, SixthPoint);
+
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", SeventhPoint);
+		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, SeventhPoint);
+        paramHandler.OnComplexValueChanged("Speed", new_speed, SeventhPoint);
+        paramHandler.OnComplexValueChanged("Accel", new_accel, SeventhPoint);
+		paramHandler.OnComplexValueChanged("Blend", new_blend, SeventhPoint);
+		paramHandler.OnComplexValueChanged("Coord Type", new_coord, SeventhPoint);
+
+		paramHandler.OnComplexValueChanged("Tool", "tcp_1", EighthPoint);
+		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, EighthPoint);
+        paramHandler.OnComplexValueChanged("Speed", new_speed, EighthPoint);
+        paramHandler.OnComplexValueChanged("Accel", new_accel, EighthPoint);
+		paramHandler.OnComplexValueChanged("Blend", new_blend, EighthPoint);
+		paramHandler.OnComplexValueChanged("Coord Type", new_coord, EighthPoint);
+
+		// Add OLP commands
+		AddOpenCloseCommand("point3op" + op_idx, "Camozzi Gripper UR5e", "CLOSE", "tf_tcp_1");
+		AddOpenCloseCommand("point6op" + op_idx, "Camozzi Gripper UR5e", "OPEN", "tf_tcp_1");
         
     }
 }
